3f7a02f8e07140972b3b31ba546619edb43acd53
Stephen <stephen.a.nerby@gmail.com>
2023-01-25 01:34:58 -0500

    Debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,52 +1,26 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
-  # index
-  get '/games' do
-    @games = Game.all
-    @games.map{|game| game}.to_json
-  end
-
-  # new
-  get '/games/new' do
-      
-  end
-
-  # create
-  post '/games' do
-    @game = Game.create(params[:game])
-  end
-
-  # show
-  get '/games/:id' do
-    puts params
-    @game = Game.find(params[:id])
-    @game.title
-  end
-
-  # edit
-  get '/games/:id/edit' do
-      
-  end
-
-  # update
-  patch '/games/:id' do
   
+  get "/games" do
+    games = Game.all
+    games.to_json
   end
-
-  put '/games/:id' do
   
+  post "/games" do
+    game = Game.create(params[:game])
+    game.to_json
   end
-
-  # delete
-  delete '/games/:id' do
-    Game.find(params[:id]).destroy!
-    redirect '/games'
+  
+  patch "/games/:id" do
+    game = Game.find(params[:id])
+    game.update(params)
+    game.to_json
   end
-
+  
+  delete "/games/:id" do
+    game = Game.find(params[:id])
+    game.destroy
+    game.to_json
+  end
+  
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,10 +2,4 @@ class Game < ActiveRecord::Base
     validates :title, { presence: true }
     validates :publisher, { presence: true }
     validates :platform, { presence: true }
-
-    def game_title
-        "#{title}"
-    end
-
-
 end
