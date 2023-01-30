9c6ca34761df46e9d2435273e1466ba78890db9b
Stephen <stephen.a.nerby@gmail.com>
2023-01-30 17:13:35 -0500

    Seed file populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,85 @@
 puts "🌱 Seeding..."
 
-#task1 -> need to put in seed data for 'wins'
-#task2 -> need to make sure wins seed data is associated with a user
+Game.create([
+    {
+        title: "RuneScape",
+        publisher: "Jagex",
+        platform_id: "3"
+    },
+    {
+        title: "Bloodborne",
+        publisher: "Sony",
+        platform_id: "1"
+    },
+    {
+        title: "Halo 3",
+        publisher: "343 Studios",
+        platform_id: "2"
+    },
+    {
+        title: "Tony Hawk's Pro Skater 3",
+        publisher: "Activision O2",
+        platform_id: "9"
+    },
+
+])
+
+Platform.create([
+    {
+        id: "1",
+        name: "Playstation"
+    },
+    {
+        id: "2",
+        name: "Xbox"
+    },
+    {
+        id: "3",
+        name: "PC"
+    },
+    {
+        id: "4",
+        name: "Switch"
+    },
+    {
+        id: "5",
+        name: "Mobile"
+    },
+    {
+        id: "6",
+        name: "NES"
+    },
+    {
+        id: "7",
+        name: "SNES"
+    },
+    {
+        id: "8",
+        name: "N64"
+    },
+    {
+        id: "9",
+        name: "Gamecube"
+    },
+    {
+        id: "10",
+        name: "Wii"
+    },
+    {
+        id: "11",
+        name: "Dreamcast"
+    },
+    {
+        id: "12",
+        name: "Steamdeck"
+    },
+    {
+        id: "13",
+        name: "Handheld"
+    },
+    {
+        id: "14",
+        name: "Other"
+    }
+])
 puts "✅ Done seeding!"
